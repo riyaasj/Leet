64859fc77bbed6809133b7c6af468e28339ec04a
Riya Seby John <riyasebyjohn@gmail.com>
2025-06-29 15:15:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (11.05%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,13 +4,13 @@ public:
         if(n == 0 || n == 1){
             return n;
         }
-        if(!m[n - 1]){
-            m[n - 1] = solve(n - 1, m) + solve(n - 2, m);
+        if(!m[n]){
+            m[n] = solve(n - 1, m) + solve(n - 2, m);
         }
-        return m[n - 1];
+        return m[n];
     }
     int fib(int n) {
-        vector<int> m(n, 0);
+        vector<int> m(n + 1, 0);
         return solve(n, m);
     }
 };
