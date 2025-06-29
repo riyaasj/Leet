55e8469c5a914a2306a09e79c986abe772983c0b
Riya Seby John <riyasebyjohn@gmail.com>
2025-06-29 15:13:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.05%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int solve(int n, unordered_map<int, int> &m){
-        if(n == 0){
-            return 0;
+    int solve(int n, vector<int> &m){
+        if(n == 0 || n == 1){
+            return n;
         }
-        if(!m[n]){
-            m[n] = solve(n - 1, m) + solve(n - 2, m);
+        if(!m[n - 1]){
+            m[n - 1] = solve(n - 1, m) + solve(n - 2, m);
         }
-        return m[n];
+        return m[n - 1];
     }
     int fib(int n) {
-        unordered_map<int, int> m;
-        m[1] = 1;
+        vector<int> m(n, 0);
         return solve(n, m);
     }
 };
