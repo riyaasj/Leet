9a79f04d0d4331efa4b0c6e3ace2cb7cbc07af67
Riya Seby John <riyasebyjohn@gmail.com>
2025-06-30 20:15:11 +0530

    Time: 735 ms (5.00%) | Memory: 644.3 MB (5.00%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void dfs(int i, int j, vector<vector<int>> &visited, vector<vector<int>> heights){
+        visited[i][j] = 1;
+        vector<vector<int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        for(auto d: dir){
+            int x = i + d[0],  y = j + d[1];
+            if(x >= 0 && x < heights.size() && y >= 0 && y < heights[0].size() && heights[x][y] >= heights[i][j] && !visited[x][y]){
+                dfs(x, y, visited, heights);
+            }
+        }
+    }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        vector<vector<int>> atlantic(heights.size(), vector<int>(heights[0].size(), 0)), pacific(heights.size(), vector<int>(heights[0].size(), 0)), res;
+        for(int i = 0; i < heights.size(); i++){
+            for(int j = 0; j < heights[0].size(); j++){
+                if((i == 0 || j == 0) && (!pacific[i][j])){
+                    dfs(i, j, pacific, heights);
+                }
+                if((i == heights.size() - 1 || j == heights[0].size() - 1) && (!atlantic[i][j])){
+                    dfs(i, j, atlantic, heights);
+                }
+            }
+        }
+        for(int i = 0; i < heights.size(); i++){
+            for(int j = 0; j < heights[0].size(); j++){
+                if(atlantic[i][j] && pacific[i][j]){
+                    res.push_back({i, j});
+                }
+            }
+        }
+        return res;
+    }
+};
