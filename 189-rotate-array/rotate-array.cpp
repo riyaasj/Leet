962325b70f0a5a6fdae40a6623b71f9e51ea70e0
Riya Seby John <riyasebyjohn@gmail.com>
2025-07-02 20:18:45 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (79.31%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        int i = 0, j = nums.size() - 1;
+        while(i < j){
+            swap(nums[i++], nums[j--]);
+        }
+        i = 0, j = k - 1;
+        while(i < j){
+            swap(nums[i++], nums[j--]);
+        }
+        i = k, j = nums.size() - 1;
+        while(i < j){
+            swap(nums[i++], nums[j--]);
+        }
+    }
+};
