fd817dff81c13d4707f37ebb27c940aa0def8755
Riya Seby John <riyasebyjohn@gmail.com>
2025-07-03 20:32:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (68.01%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char, int> m;
-        for(char x: s1){
-            m[x]++;
+        if(s2.size() < s1.size()) return false;
+
+        vector<int> m(26, 0), t(26, 0);
+
+        for(int i = 0; i < s1.size(); i++){
+            t[s2[i] - 'a']++;
+            m[s1[i] - 'a']++;
         }
-        int i = 0, j = s1.size() - 1, res = 0;
-        while(j < s2.size()){
-            unordered_map<char, int> temp = m;
-            for(int k = i; k <= j; k++){
-                if(temp[s2[k]] == 0){
-                    break;
-                }
-                temp[s2[k]]--;
+
+        int matches = 0;
+        for(int i = 0; i < 26; i++){
+            if(t[i] == m[i]) matches++;
+        }
+
+        if(matches == 26) return true;
+
+        int i = 0, j = s1.size() - 1;
+        while(j < s2.size() - 1){
+            t[s2[i] - 'a']--;
+            if(t[s2[i] - 'a'] == m[s2[i] - 'a']){
+                matches++;
             }
-            int flag = 1;
-            for(auto a: temp){
-                if(a.second > 0){
-                    flag = 0;
-                    break;
-                }
+            else if(t[s2[i] - 'a'] == m[s2[i] - 'a'] - 1){
+                matches--;
             }
-            res += flag;
-            j++;
             i++;
+
+            j++;
+            t[s2[j] - 'a']++;
+            if(t[s2[j] - 'a'] == m[s2[j] - 'a']){
+                matches++;
+            }
+            else if(t[s2[j] - 'a'] == m[s2[j] - 'a'] + 1){
+                matches--;
+            }
+            if(matches == 26) return true;
         }
-        return res;
+        return false;
     }
 };
