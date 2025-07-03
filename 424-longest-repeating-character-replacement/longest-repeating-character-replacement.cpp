6c4b5761fc047aa0125cc5764eadbf9cfd995105
Riya Seby John <riyasebyjohn@gmail.com>
2025-07-03 14:58:19 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (40.61%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    int findMax(vector<int> arr){
-        int res = arr[0];
-        for(int i = 1; i < arr.size(); i++){
-            if(arr[i] > res) res = arr[i];
-        }
-        return res;
-    }
     int characterReplacement(string s, int k) {
         int left = 0, res = 0, maxFreq = 0;
         vector<int> c(26, 0);
