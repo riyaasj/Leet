2cf7fe565613e1a3ab1638752a66f12374d02a0f
Riya Seby John <riyasebyjohn@gmail.com>
2025-07-03 15:44:53 +0530

    Time: 19 ms (30.13%) | Memory: 40.4 MB (6.60%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        m[0] = 1;
+        int sum = 0, res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            res += m[sum - k];
+            m[sum]++;
+        }
+        return res;
+    }
+};
