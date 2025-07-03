6998629131235ebbc6c8ee737daf43e70158c0bb
Riya Seby John <riyasebyjohn@gmail.com>
2025-07-03 14:55:02 +0530

    Time: 54 ms (5.07%) | Memory: 46.9 MB (5.45%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -3,9 +3,7 @@ public:
     int findMax(vector<int> arr){
         int res = arr[0];
         for(int i = 1; i < arr.size(); i++){
-            if(arr[i] > res){
-                res = arr[i];
-            }
+            if(arr[i] > res) res = arr[i];
         }
         return res;
     }
@@ -13,11 +11,9 @@ public:
         int left = 0, res = 0, maxFreq = 0;
         vector<int> c(26, 0);
         for(int right = 0; right < s.size(); right++){
-            c[s[right] - 'A']++;
-            maxFreq = max(maxFreq, c[s[right] - 'A']);
+            maxFreq = max(maxFreq, ++c[s[right] - 'A']);
             while((right - left + 1) - maxFreq > k){
-                c[s[left] - 'A']--;
-                left++;
+                c[s[left++] - 'A']--;
                 maxFreq = findMax(c);
             }
             res = max(res, right - left + 1);
