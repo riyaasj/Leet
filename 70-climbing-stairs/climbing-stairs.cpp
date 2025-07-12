f639d6237b69dc288887f3fe839698e880e6dcb2
Riya Seby John <riyasebyjohn@gmail.com>
2025-07-12 15:04:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.42%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2){
+            return n;
+        }
+        int a = 1, b = 1;
+        for(int i = 2; i <= n; i++){
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+};
