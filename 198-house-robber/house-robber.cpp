720755117d701f9148b000a9cea2c1ae46a24d18
Riya Seby John <riyasebyjohn@gmail.com>
2025-07-13 12:10:26 +0530

    Time: 2 ms (7.58%) | Memory: 9.9 MB (97.33%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,7 +4,7 @@ public:
         int a = 0, b = nums[0];
         for(int i = 1; i <= nums.size() - 1; i++){
             int s1 = nums[i], s2 = b;
-            if(i > 1) s1 += a;
+s1 += a;
             int next = max(s1, s2);
             a = b;
             b = next;
