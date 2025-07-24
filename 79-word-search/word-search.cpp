e1b3bc6df7f793466d640a5301b7aef7bb6a935e
Riya Seby John <riyasebyjohn@gmail.com>
2025-07-24 20:39:10 +0530

    Time: 832 ms (24.04%) | Memory: 11.6 MB (45.95%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    int search(vector<vector<char>>& board, string word, int index, int i, int j, vector<vector<int>>& vis){
+    int dfs(vector<vector<char>>& board, string word, int index, int i, int j){
         if(index == word.size()){
             return 1;
         }
-        if(i >= board.size() || i < 0 || j >= board[0].size() || j < 0 || vis[i][j]){
+        if(i >= board.size() || i < 0 || j >= board[0].size() || j < 0 || board[i][j] != word[index]){
             return 0;
         }
-        int res = 0;
-        if(word[index] == board[i][j]){
-            vis[i][j] = 1;
-            res = search(board, word, index + 1, i + 1, j, vis)
-            || search(board, word, index + 1, i - 1, j, vis)
-            || search(board, word, index + 1, i, j + 1, vis)
-            || search(board, word, index + 1, i, j - 1, vis);
-        }
-        vis[i][j] = 0;
-        return res;
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        bool found = dfs(board, word, index + 1, i + 1, j) ||
+                     dfs(board, word, index + 1, i - 1, j) ||
+                     dfs(board, word, index + 1, i, j + 1) ||
+                     dfs(board, word, index + 1, i, j - 1);
+
+        board[i][j] = temp;
+        return found;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size(), m = board[0].size();
         for(int i = 0; i < board.size(); i++){
             for(int j = 0; j < board[0].size(); j++){
                 if(board[i][j] == word[0]){
-                    vector<vector<int>> vis(n, vector<int>(m, 0));
-                    if(search(board, word, 0, i, j, vis)){
+                    if(dfs(board, word, 0, i, j)){
                         return true;
                     }
                 }
