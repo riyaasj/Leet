62115ba544fc5da4d5beca794f908c2870ec135e
Riya Seby John <riyasebyjohn@gmail.com>
2025-07-24 20:33:39 +0530

    Time: 896 ms (12.68%) | Memory: 11.9 MB (17.69%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int search(vector<vector<char>>& board, string word, int index, int i, int j, vector<vector<int>>& vis){
+        if(i >= board.size() || i < 0 || j >= board[0].size() || j < 0 || vis[i][j]){
+            return 0;
+        }
+        int res = 0;
+        if(index == word.size() - 1){
+            if(board[i][j] == word[index]){
+                return 1;
+            }
+            return 0;
+        }
+        else if(word[index] == board[i][j]){
+            vis[i][j] = 1;
+            res = search(board, word, index + 1, i + 1, j, vis)
+            || search(board, word, index + 1, i - 1, j, vis)
+            || search(board, word, index + 1, i, j + 1, vis)
+            || search(board, word, index + 1, i, j - 1, vis);
+        }
+        vis[i][j] = 0;
+        return res;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size(), m = board[0].size();
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board[0].size(); j++){
+                vector<vector<int>> vis(n, vector<int>(m, 0));
+                if(search(board, word, 0, i, j, vis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
