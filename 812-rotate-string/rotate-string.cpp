9ae0af47132d3197b3789f632b7b092938d4e1df
Riya Seby John <riyasebyjohn@gmail.com>
2025-08-03 14:56:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (7.05%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,8 +3,9 @@ public:
     bool rotateString(string s, string goal) {
         if(s.size() != goal.size()) return false;
         for(int i = 0; i < s.size(); i++){
-            string temp = s.substr(i, s.size() - i) + s.substr(0, i);
-            if(temp == goal) return true;
+            stringstream temp;
+            temp << s.substr(i, s.size() - i) << s.substr(0, i);
+            if(temp.str() == goal) return true;
         }
         return false;
     }
