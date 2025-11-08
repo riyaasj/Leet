c32e866c321a6a92f3da1620431e48266abaf5e9
Riya Seby John <riyasebyjohn@gmail.com>
2025-11-08 08:22:07 +0530

    Time: 15 ms (17.75%) | Memory: 44.7 MB (74.39%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        if(s.size() == 0){
-            return 0;
-        }
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-        int greed = 0, num = 0;
-
-        for(int cookie = 0; cookie < s.size(); cookie++){
-            if(s[cookie] >= g[greed]){
-                num++;
-                greed++;
-                if(greed == g.size()) return num;
+        int child = 0, cookie = 0;
+        while(child < g.size() && cookie < s.size()){
+            if(g[child] <= s[cookie]){
+                child++;
             }
+            cookie++;
         }
-        return num;
+        return child;
     }
 };
