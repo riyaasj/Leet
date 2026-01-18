5d2c53ce9c45763bf7887d73472460be49b1f976
Riya Seby John <riyasebyjohn@gmail.com>
2026-01-18 09:47:45 +0530

    Time: 2 ms (89.51%) | Memory: 47.8 MB (64.38%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = -1;
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != 0){
+                nums[++i] = nums[j];
+            }
+        }
+        i++;
+        for(; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
