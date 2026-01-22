87bf1c2f4411723b4a18c10335cb51d0b17d1804
Riya Seby John <riyasebyjohn@gmail.com>
2026-01-22 08:12:54 +0530

    Time: 4 ms (73.59%) | Memory: 45.2 MB (53.63%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Deque<Integer> s = new ArrayDeque<>();
+        Deque<Integer> d = new ArrayDeque<>();
         Map<Integer, Integer> m = new HashMap<>();
         for(int i = nums2.length - 1; i >= 0; i--){
-            while(!s.isEmpty() && s.peek() <= nums2[i]){
-                s.pop();
+            while(!d.isEmpty() && d.peek() < nums2[i]){
+                d.pop();
             }
-            if(s.isEmpty()){
+            if(d.isEmpty()){
                 m.put(nums2[i], -1);
             }
             else{
-                m.put(nums2[i], s.peek());
+                m.put(nums2[i], d.peek());
             }
-            s.push(nums2[i]);
+            d.push(nums2[i]);
         }
         for(int i = 0; i < nums1.length; i++){
             nums1[i] = m.get(nums1[i]);
